17bfa82036eadd81dfca989857077bf74f920f4d
Denis Tarasov <dstarasov@skbkontur.ru>
2021-06-16 10:10:04 +0500

    feat: добавили стрельбу

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import sys
 
 def make_draft(data: dict) -> dict:
     draft = {}
@@ -9,7 +8,7 @@ def make_draft(data: dict) -> dict:
 
 def make_turn(data: dict) -> dict:
     battle_output = {
-        'Message': f"I have {len(data['My'])} ships and move to center of galaxy",
+        'Message': f"I have {len(data['My'])} ships and move to center of galaxy and shoot",
         'UserCommands': []
     }
     for ship in data['My']:
@@ -20,7 +19,19 @@ def make_turn(data: dict) -> dict:
                 'Target': '15/15/15'
             }
         })
+        guns = [x for x in ship['Equipment'] if x['Type'] == 1]
+        if guns:
+            gun = guns[0]
+            battle_output['UserCommands'].append({
+                "Command": "ATTACK",
+                "Parameters": {
+                    'Id': ship['Id'],
+                    'Name': gun['Name'],
+                    'Target': '15/15/15'
+                }
+            })
     return battle_output
+
 
 def play_game():
     while True:
@@ -30,6 +41,7 @@ def play_game():
             print(json.dumps(make_draft(line), ensure_ascii=False))
         elif 'My' in line:
             print(json.dumps(make_turn(line), ensure_ascii=False))
+
 
 if __name__ == '__main__':
     play_game()
